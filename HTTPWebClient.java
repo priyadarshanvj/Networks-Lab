ab9f3569a63141c2c3ac180c4d4db924d8672806
priyadarshanvj <63783259+priyadarshanvj@users.noreply.github.com>
2021-12-24 12:28:59 +0530

    Add files via upload

---
+++ HTTPWebClient.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+public class HTTPWebClient
+{
+	public static void main(String[] args)
+	{
+		String hostName = "www.twitter.com";
+		int portNumber = 80;
+		try
+		{
+			Socket socket = new Socket(hostName, portNumber);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out.println("GET / HTTP/1.1\nHost:www.twitter.com\n\n");
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+			{
+				System.out.println(inputLine);
+			}
+		}		
+		catch (UnknownHostException e)
+		{
+			System.err.println("Don't know about host "+hostName);
+			System.exit(1);
+		}
+		catch (IOException e)
+		{
+			System.err.println("Couldn't get I/O for the connection to "+hostName);
+			System.exit(1);
+		}
+	}
+}
